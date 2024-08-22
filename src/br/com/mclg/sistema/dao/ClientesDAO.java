4ec983e575d3c27d880f247f034ade896e57732d
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2024-08-22 18:11:59 -0300

    Criando o método listar clientes.

--- src/br/com/mclg/sistema/dao/ClientesDAO.java
+++ src/br/com/mclg/sistema/dao/ClientesDAO.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class ClientesDAO {
@@ -77,6 +79,37 @@ public class ClientesDAO {
             return obj;
         } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "ERRRO ao buscar cliente!"+erro);
+        }
+        return null;
+    }
+    public List<Clientes> listar(){
+        List<Clientes> lista = new ArrayList<>();
+        try {
+            String sql = "select * from tb_clientes";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                Clientes obj = new Clientes();
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setRg(rs.getString("rg"));
+                obj.setCpf(rs.getString("cpf"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setEstado(rs.getString("estado"));  
+                lista.add(obj);
+            }
+            return lista;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "ERRO so criar a lista: "+e);
         }
         return null;
     }
